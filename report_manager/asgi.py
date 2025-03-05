dde811918bfb55856603ba320551124e5b38edb3
Ojo-Oluwagbenga <ojojohn2907@gmail.com>
2025-03-05 04:27:02 +0100

    General Commit

--- report_manager/asgi.py
+++ report_manager/asgi.py
@@ -14,3 +14,25 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'report_manager.settings')
 
 application = get_asgi_application()
+
+
+'''
+[Unit]
+Description=report_manager daemon
+Requires=report_manager.socket
+After=network.target
+
+[Service]
+User=rider
+Group=www-data
+WorkingDirectory=/home/rider/projects/report_manager
+ExecStart=/home/rider/projects/oneklassenv/bin/gunicorn \
+          --access-logfile - \
+          -k uvicorn.workers.UvicornWorker \
+          --workers 3 \
+          --bind unix:/run/report_manager.sock \
+          report_manager.asgi:application
+
+[Install]
+WantedBy=multi-user.target
+'''
